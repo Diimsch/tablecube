f1549b93144a4ab831cd4102ad23cf61e5a064f4
Adrian Busse <git@adrianmxb.com>
2021-12-13 18:47:52 +0100

    feat: add more backend functionality and seed data

---
+++ src/backend/src/resolvers/booking.ts
@@ -0,0 +1,323 @@
+import { Booking, BookingStatus, MenuItem, UserRole } from "@prisma/client";
+import { AuthenticationError, UserInputError } from "apollo-server-errors";
+import ms from "ms";
+import { Resolvers } from "../generated/graphql";
+import { generateValidatorCode } from "../utils";
+
+export const bookingResolvers: Resolvers = {
+  BookingItem: {
+    item: async (parent, args, ctx) => {
+      const item = await ctx.prisma.menuItem.findUnique({
+        where: {
+          id: parent.itemId,
+        },
+      });
+      // the exclamation mark tells typescript that this field is safe to interpret as never null.
+      return item!;
+    },
+  },
+  Booking: {
+    table: async (parent, args, ctx) => {
+      const table = await ctx.prisma.table.findUnique({
+        where: {
+          id: parent.tableId,
+        },
+      });
+      return table!;
+    },
+    items: async (parent, args, ctx) => {
+      const items = await ctx.prisma.itemsOnBooking.findMany({
+        where: {
+          bookingId: parent.id,
+        },
+        include: {
+          item: true,
+        },
+      });
+      return items;
+    },
+  },
+  Query: {
+    booking: async (parent, args, ctx): Promise<Booking> => {
+      if (!ctx.token.userId) {
+        throw new AuthenticationError("not signed in");
+      }
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const booking = await ctx.prisma.booking.findUnique({
+        where: {
+          id: args.bookingId,
+        },
+        include: {
+          users: true,
+        },
+      });
+
+      if (
+        booking?.users.find((u) => u.userId !== user.id) &&
+        user.rolesInRestaurants.find(
+          (r) => r.restaurantId !== booking?.restaurantId
+        )
+      ) {
+        throw new UserInputError("not allowed to receive this booking");
+      }
+      return booking as Booking;
+    },
+  },
+  Mutation: {
+    createBooking: async (parent, args, ctx) => {
+      if (!ctx.token.userId) {
+        throw new AuthenticationError("not signed in");
+      }
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const validatorColorCode = generateValidatorCode();
+
+      const current = new Date();
+      if (!args.booking.start) {
+        args.booking.start = current;
+      }
+
+      if (!args.booking.end) {
+        args.booking.end = new Date(current.getTime() + ms("1h"));
+      }
+
+      if (
+        args.booking.start.getTime() < current.getTime() ||
+        args.booking.end.getTime() < current.getTime()
+      ) {
+        throw new UserInputError("start or end date smaller than current date");
+      }
+
+      const bookings = await ctx.prisma.booking.findMany({
+        where: {
+          tableId: args.booking.tableId,
+          status: {
+            not: "DONE",
+          },
+          OR: [
+            {
+              start: {
+                lt: args.booking.start,
+              },
+              end: {
+                gte: args.booking.start,
+              },
+            },
+            {
+              AND: [
+                {
+                  start: {
+                    gte: args.booking.start,
+                  },
+                },
+                {
+                  start: {
+                    lte: args.booking.end,
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      });
+
+      if (bookings.length > 0) {
+        throw new UserInputError(
+          "table currently still being served or already reserved"
+        );
+      }
+
+      const booking = await ctx.prisma.booking.create({
+        data: {
+          tableId: args.booking.tableId,
+          start: args.booking.start,
+          end: args.booking.end,
+          restaurantId: args.booking.restaurantId,
+          joinValidationData: validatorColorCode,
+          status: BookingStatus.RESERVED,
+          users: {
+            create: {
+              userId: user.id,
+              role: "HOST",
+            },
+          },
+        },
+      });
+      console.log(`validation data for ${booking.id}: ${validatorColorCode}`);
+
+      return booking;
+    },
+    joinBooking: async (parent, args, ctx) => {
+      if (!ctx.token.userId) {
+        throw new AuthenticationError("not signed in");
+      }
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const booking = await ctx.prisma.booking.findFirst({
+        where: {
+          tableId: args.data.tableId,
+          status: BookingStatus.CHECKED_IN,
+        },
+      });
+
+      if (!booking) {
+        throw new UserInputError("no ready booking at table");
+      }
+
+      if (
+        args.data.colorCode.toString() !==
+        booking.joinValidationData?.toString()
+      ) {
+        throw new UserInputError("invalid color validation code");
+      }
+
+      await ctx.prisma.usersAtBooking.create({
+        data: {
+          bookingId: booking.id,
+          role: "GUEST",
+          userId: user.id,
+        },
+      });
+
+      return booking;
+    },
+    addItemToBooking: async (parent, args, ctx) => {
+      if (!ctx.token.userId) {
+        throw new AuthenticationError("not signed in");
+      }
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const isAtBooking = await ctx.prisma.usersAtBooking.findUnique({
+        where: {
+          userId_bookingId: {
+            bookingId: args.data.bookingId,
+            userId: user.id,
+          },
+        },
+      });
+
+      if (!isAtBooking) {
+        throw new UserInputError(
+          "booking doesn't exist or user not part of booking"
+        );
+      }
+
+      await ctx.prisma.itemsOnBooking.create({
+        data: {
+          bookingId: args.data.bookingId,
+          itemId: args.data.itemId,
+          comment: args.data.comment,
+        },
+      });
+
+      return ctx.prisma.booking.findUnique({
+        where: {
+          id: args.data.bookingId,
+        },
+      });
+    },
+    checkIn: async (parent, args, ctx) => {
+      if (!ctx.token.userId) {
+        throw new AuthenticationError("not signed in");
+      }
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const current = new Date();
+      const nextBooking = await ctx.prisma.booking.findFirst({
+        where: {
+          tableId: args.tableId,
+          status: "RESERVED",
+          start: {
+            lte: current,
+          },
+          end: {
+            gte: current,
+          },
+        },
+        orderBy: {
+          start: "asc",
+        },
+      });
+
+      if (!nextBooking) {
+        throw new UserInputError("no open booking found");
+      }
+
+      if (nextBooking.joinValidationData?.toString() !== args.code.toString()) {
+        throw new UserInputError("invalid color validation code");
+      }
+
+      const booking = await ctx.prisma.booking.update({
+        where: {
+          id: nextBooking.id,
+        },
+        data: {
+          status: "CHECKED_IN",
+        },
+      });
+
+      return booking;
+    },
+  },
+};
---
+++ src/backend/src/resolvers/table.ts
@@ -0,0 +1,48 @@
+import { BookingStatus, MenuItem, UserRole } from "@prisma/client";
+import { AuthenticationError, UserInputError } from "apollo-server-errors";
+import { Resolvers } from "../generated/graphql";
+import { PubSub } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
+
+export const tablesResolvers: Resolvers = {
+  Subscription: {
+    validationPrompted: {
+      async *subscribe() {
+        yield pubsub.asyncIterator('VALIDATION_PROMPTED');
+      },
+      resolve: (msg: any) => {
+        return msg;
+      }
+    }
+  },
+  Query: {
+    promptValidation: async(parent, args, ctx) => {
+      if(ctx.token.userId !== "TABLE") {
+        throw new AuthenticationError("only accessible by table control");
+      }
+      
+      const booking = await ctx.prisma.booking.findFirst({
+        where: {
+          tableId: args.tableId,
+          NOT: {
+            OR: [
+              {
+                status: BookingStatus.RESERVED
+              },
+              {status: BookingStatus.DONE}
+            ]
+          }
+        }
+      });
+
+      if(!booking) {
+        return false;
+      }
+
+      pubsub.publish("VALIDATION_PROMPTED", booking?.joinValidationData);
+      return true;
+    }
+  },
+  Mutation: {},
+};
