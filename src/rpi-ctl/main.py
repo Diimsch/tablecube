d1cbabad0c3b52cc97858aa4fddfd0b6e0e6c619
Adrian Busse <git@adrianmxb.com>
2022-01-05 11:46:01 +0100

    wip

--- src/rpi-ctl/main.py
+++ src/rpi-ctl/main.py
@@ -64,6 +64,7 @@ async def main():
                 keybow.show()
                 await asyncio.sleep(2)
             keybow.clear()
+            keybow.show()
             await asyncio.sleep(3)
 
 '''
