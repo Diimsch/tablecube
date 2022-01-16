dab3123c3a7573c19f972528074fb310d8c555bc
Adrian Busse <git@adrianmxb.com>
2022-01-16 19:14:09 +0100

    feat: add addItemToTable mutation

--- src/backend/src/resolvers/table.ts
+++ src/backend/src/resolvers/table.ts
@@ -31,7 +31,6 @@ export const tablesResolvers: Resolvers = {
     },
     occupyingBooking: async (parent, args, ctx) => {
       const current = new Date();
-      console.log(current);
       const booking = await ctx.prisma.booking.findFirst({
         where: {
           tableId: parent.id,
@@ -129,5 +128,85 @@ export const tablesResolvers: Resolvers = {
       return true;
     },
   },
-  Mutation: {},
+  Mutation: {
+    addItemToTable: async (parent, args, ctx) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.token.userId!,
+        },
+        include: {
+          rolesInRestaurants: true,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("invalid user");
+      }
+
+      const current = new Date();
+      const booking = await ctx.prisma.booking.findFirst({
+        where: {
+          tableId: args.data.tableId,
+          OR: [
+            {
+              status: {
+                notIn: ["DONE", "RESERVED"]
+              }
+            },
+            {
+              status: {
+                not: "DONE",
+              },
+              start: {
+                lt: current
+              },
+              end: {
+                gte: current
+              },
+            }
+          ]
+        },
+      });
+
+      if (!booking) {
+        throw new UserInputError(
+          "booking doesn't exist or user not part of booking"
+        );
+      }
+
+      const role = user.rolesInRestaurants.find(
+        (userRole) => userRole.restaurantId === booking.restaurantId
+      );
+      if (!role || role.role === "NONE") {
+        const isAtBooking = await ctx.prisma.usersAtBooking.findUnique({
+          where: {
+            userId_bookingId: {
+              bookingId: booking.id,
+              userId: user.id,
+            },
+          },
+        });
+
+        if (!isAtBooking) {
+          throw new UserInputError(
+            "booking doesn't exist or user not part of booking"
+          );
+        }
+      }
+
+      await ctx.prisma.itemsOnBooking.create({
+        data: {
+          bookingId: booking.id,
+          itemId: args.data.itemId,
+          comment: args.data.comment,
+        },
+      });
+
+      return ctx.prisma.booking.findUnique({
+        where: {
+          id: booking.id,
+        },
+      });
+    },
+  },
 };
